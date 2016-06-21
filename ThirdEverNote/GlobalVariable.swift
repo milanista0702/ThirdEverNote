c074e40062827a03823cbda98f7724544b849d2d
makka misako <onedirec1205@gmail.com>
2016-06-21 20:52:59 +0900

    [Change] Appkey

---
+++ ThirdEverNote/GlobalVariable.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalVariable.swift
+//  ThirdEverNote
+//
+//  Created by makka misako on 2016/06/21.
+//  Copyright © 2016年 makka misako. All rights reserved.
+//
+
+import UIKit
+
+class GlobalVariable: NSObject {
+    static let appKey = "a499ff4b312929d9985d62a52f573a34aeead360e2bc03172d9ccf513bf7dc46"
+    static let clientKey = "64a32d1c8a30fc3bcbd350a6d942111a5ac406cf1f3066e5d157eac176261fec"
+}
