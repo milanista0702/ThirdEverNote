460fed69b1d34622237ee11732730194d1d2bf69
makka misako <onedirec1205@gmail.com>
2016-08-09 15:47:09 +0900

    一応

--- ThirdEverNote/ToDoes.swift
+++ ThirdEverNote/ToDoes.swift
@@ -6,57 +6,123 @@
 //  Copyright © 2016年 makka misako. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import NCMB
 
-class ToDoes: NCMBObject, NCMBSubclassing {
-    @NSManaged var todo: String! //内容
-    @NSManaged var user: NCMBUser! //誰のか
-    @NSManaged var isPublic: NSNumber!  //共有するかどうか
-    @NSManaged var date: NSDate!        //期限
-    @NSManaged var done: NSNumber!      //もうやったかどうか
-    
-    
-    
-    init(todo: String, user: NCMBUser, isPublic: NSNumber, date: NSDate) {
-        super.init()
-        self.todo = todo
-        self.user = user
-        self.isPublic = isPublic
-        self.date = date
-        self.done = 0
-    }
-    
-    override init() {
-        super.init()
-    }
-    
-    static func ncmbClassName() -> String! {
-        return "ToDoes"
-    }
-    
-    static func create(titleOfToDoes todo: String, user: NCMBUser, isPublic: NSNumber, date: NSDate) {
-        let todos:ToDoes = ToDoes(todo: todo, user: user, isPublic: isPublic, date: date)
-        todos.saveEventually { (error) in
-            if error != nil {
-                print("\(error.localizedDescription)")
-            }
+@objc(ToDoes)
+class ToDoes: NCMBObject, NCMBSubclassing{
+    var todo: String! {
+        get {
+            return objectForKey("todo") as! String
+        }
+        set {
+            setObject(newValue, forKey: "todo")
         }
     }
     
-    static func loadAll() -> [ToDoes] {
-        var todoes: [ToDoes] = []
-        let query = NCMBQuery(className: self.ncmbClassName())
-        query.findObjectsInBackgroundWithBlock { (objects, error) in
-            if error == nil {
-                for todo in objects {
-                    todoes.append(todo as! ToDoes)
-                }
-            }else {
-                print("\(error.localizedDescription)")
-            }
+    var user: NCMBUser {
+        get  {
+            return objectForKey("user") as!  NCMBUser
         }
-        return todoes
+        set {
+            setObject(newValue, forKey: "user")
+        }
     }
+    
+    var isPublic: Int {
+        get {
+            return objectForKey("isPublic") as! Int
+        }
+        set {
+            setObject(newValue, forKey: "isPublic")
+        }
+    }
+    
+    var date: NSDate {
+        get {
+            return objectForKey("date") as! NSDate
+        }
+        set {
+            setObject(newValue, forKey: "date")
+        }
+    }
+    
+    var done: Int {
+        get {
+            return objectForKey("done") as! Int
+        }
+        set {
+            setObject(newValue, forKey: "done")
+        }
+    }
+    
+    //呼び出すときのclassNameを設定
+    override init!(className: String!) {
+        super.init(className: className)
+    }
+    
+    static func create(todo: String, user: NCMBUser, isPublic: Int, date: NSDate, done: Int) -> ToDoes{
+        let toDoes = ToDoes(className: "ToDoes")
+        toDoes.todo = todo
+        toDoes.user = user
+        toDoes.isPublic = isPublic
+        toDoes.date = date
+        toDoes.done = Int
+    }
+    
 }
 
+//import UIKit
+//import NCMB
+//
+//class ToDoes: NCMBObject, NCMBSubclassing {
+//    @NSManaged var todo: String! //内容
+//    @NSManaged var user: NCMBUser! //誰のか
+//    @NSManaged var isPublic: NSNumber!  //共有するかどうか
+//    @NSManaged var date: NSDate!        //期限
+//    @NSManaged var done: NSNumber!      //もうやったかどうか
+//
+//
+//
+//    init(todo: String, user: NCMBUser, isPublic: NSNumber, date: NSDate) {
+//        super.init()
+//        self.todo = todo
+//        self.user = user
+//        self.isPublic = isPublic
+//        self.date = date
+//        self.done = 0
+//    }
+//
+//    override init() {
+//        super.init()
+//    }
+//
+//    static func ncmbClassName() -> String! {
+//        return "ToDoes"
+//    }
+//
+//    static func create(titleOfToDoes todo: String, user: NCMBUser, isPublic: NSNumber, date: NSDate) {
+//        let todos:ToDoes = ToDoes(todo: todo, user: user, isPublic: isPublic, date: date)
+//        todos.saveEventually { (error) in
+//            if error != nil {
+//                print("\(error.localizedDescription)")
+//            }
+//        }
+//    }
+//
+//    static func loadAll() -> [ToDoes] {
+//        var todoes: [ToDoes] = []
+//        let query = NCMBQuery(className: self.ncmbClassName())
+//        query.findObjectsInBackgroundWithBlock { (objects, error) in
+//            if error == nil {
+//                for todo in objects {
+//                    todoes.append(todo as! ToDoes)
+//                }
+//            }else {
+//                print("\(error.localizedDescription)")
+//            }
+//        }
+//        return todoes
+//    }
+//}
+//
