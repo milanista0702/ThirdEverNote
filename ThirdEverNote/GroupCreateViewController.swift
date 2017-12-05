3147a762eafde1f4507541f06f9dfe65981850d7
milanista0702 <onedirec1205@gmail.com>
2017-12-05 20:09:35 +0900

    検索の形だけできた

--- ThirdEverNote/GroupCreateViewController.swift
+++ ThirdEverNote/GroupCreateViewController.swift
@@ -9,19 +9,39 @@
 import UIKit
 import NCMB
 
-class GroupCreateViewController: UIViewController, UIViewControllerTransitioningDelegate {
+class GroupCreateViewController: UIViewController, UIViewControllerTransitioningDelegate, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     
     @IBOutlet var text: UITextField!
     @IBOutlet var table: UITableView!
-
+    var searchController = UISearchController()
+    
+    var numberArray = ["0", "1", "2", "3"]
+    var searchResults : [String] = []
     
     @IBOutlet var createlabel: UILabel!
     @IBOutlet var namelabel: UILabel!
     @IBOutlet var memberlabel: UILabel!
-
+    
+    let saveData: UserDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         text.delegate = self
+        table.delegate = self
+        table.dataSource = self
+        
+        self.table.estimatedRowHeight = 90
+        self.table.rowHeight = UITableViewAutomaticDimension
+        table.register(UINib(nibName:"GroupTableCell", bundle: nil), forCellReuseIdentifier: "GroupTableViewCell")
+        
+        self.view.addSubview(table)
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.sizeToFit()
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        table.tableHeaderView = searchController.searchBar
         
         createlabel.backgroundColor = ColorManager.navy
         createlabel.textColor = UIColor.white
@@ -31,13 +51,48 @@ class GroupCreateViewController: UIViewController, UIViewControllerTransitioning
         memberlabel.textColor = UIColor.white
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive {
+            return searchResults.count
+        }else{
+            return numberArray.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupTableViewCell", for: indexPath) as!GroupTableViewCell
+        if searchController.isActive{
+            cell.searchlabel.text = searchResults[indexPath.row]
+        }else{
+            cell.searchlabel.text = numberArray[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("%dが選択された", numberArray[indexPath.row])
+        let cell = tableView.cellForRow(at: indexPath) as! GroupTableViewCell
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        self.searchResults = numberArray.filter{
+            $0.lowercased().contains(searchController.searchBar.text!.lowercased())
+        }
+        self.table.reloadData()
+    }
+    
     @IBAction func ok() {
-
+        
     }
     
     @IBAction func cancel() {
--- ThirdEverNote/SearchGroupViewController.swift
+++ ThirdEverNote/SearchGroupViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 import NCMB
 
-class SearchGroupViewController: UIViewController, UIViewControllerTransitioningDelegate, UITableViewDelegate,UITableViewDataSource {
-    
+class SearchGroupViewController: UIViewController, UIViewControllerTransitioningDelegate, UITableViewDelegate,UITableViewDataSource, UISearchResultsUpdating {
+
     @IBOutlet var table: UITableView!
-    var searchBar = UISearchBar()
-    var searchContrller = UISearchController()
+    var searchController = UISearchController()
     
     var numberArray = ["0", "1", "2", "3"]
+    var searchResults : [String] = []
     
     let saveData: UserDefaults = UserDefaults.standard
     
@@ -28,8 +28,16 @@ class SearchGroupViewController: UIViewController, UIViewControllerTransitioning
         
         self.table.estimatedRowHeight = 90
         self.table.rowHeight = UITableViewAutomaticDimension
-        
         table.register(UINib(nibName: "GroupTableCell", bundle: nil), forCellReuseIdentifier: "GroupTableViewCell")
+        
+        self.view.addSubview(table)
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.sizeToFit()
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        table.tableHeaderView = searchController.searchBar
         
     }
     
@@ -40,19 +48,39 @@ class SearchGroupViewController: UIViewController, UIViewControllerTransitioning
     
     // cellの数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return numberArray.count
+        if searchController.isActive {
+            return searchResults.count
+        }else{
+            return numberArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GroupTableViewCell", for: indexPath) as!GroupTableViewCell
-        cell.searchlabel.text = numberArray[indexPath.row]
+        if searchController.isActive{
+            cell.searchlabel.text = searchResults[indexPath.row]
+        }else{
+            cell.searchlabel.text = numberArray[indexPath.row]
+        }
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
+    
+    
     func tableView(_ tableView:UITableView, didSelecRowAt indexPath: IndexPath) {
         NSLog("%@が選択された", numberArray[indexPath.row])
         let cell = tableView.cellForRow(at: indexPath) as! GroupTableViewCell
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        self.searchResults = numberArray.filter{
+            $0.lowercased().contains(searchController.searchBar.text!.lowercased())
+        }
+        self.table.reloadData()
     }
     
     @IBAction func ok() {
